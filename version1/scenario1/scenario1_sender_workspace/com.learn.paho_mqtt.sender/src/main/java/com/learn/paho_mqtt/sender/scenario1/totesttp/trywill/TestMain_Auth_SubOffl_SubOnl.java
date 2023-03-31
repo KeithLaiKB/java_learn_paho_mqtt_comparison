262da827e53e2b1c169639cd04e96cc300372649
KeithLaiKB <laidelivery@163.com>
2023-03-31 01:09:18 -0400

    update wait

--- version1/scenario1/scenario1_sender_workspace/com.learn.paho_mqtt.sender/src/main/java/com/learn/paho_mqtt/sender/scenario1/totesttp/trywill/TestMain_Auth_SubOffl_SubOnl.java
+++ version1/scenario1/scenario1_sender_workspace/com.learn.paho_mqtt.sender/src/main/java/com/learn/paho_mqtt/sender/scenario1/totesttp/trywill/TestMain_Auth_SubOffl_SubOnl.java
@@ -1,4 +1,6 @@
-package com.learn.paho_mqtt.sender.scenario1.totesttp;
+package com.learn.paho_mqtt.sender.scenario1.totesttp.trywill;
+
+import java.util.ArrayList;
 
 import org.eclipse.paho.mqttv5.client.DisconnectedBufferOptions;
 import org.eclipse.paho.mqttv5.client.IMqttToken;
@@ -9,6 +11,8 @@ import org.eclipse.paho.mqttv5.client.persist.MemoryPersistence;
 import org.eclipse.paho.mqttv5.client.persist.MqttDefaultFilePersistence;
 import org.eclipse.paho.mqttv5.common.MqttException;
 import org.eclipse.paho.mqttv5.common.MqttMessage;
+import org.eclipse.paho.mqttv5.common.packet.MqttProperties;
+import org.eclipse.paho.mqttv5.common.packet.UserProperty;
 /**
  * 
  * @author laipl
@@ -77,7 +81,8 @@ public class TestMain_Auth_SubOffl_SubOnl {
         String content      = "Hello_World!";
         int qos             = 1;
 
-        String brokerUri    = "tcp://192.168.239.137:1883";							
+        String brokerUri    = "tcp://192.168.239.137:1883";	
+       // String brokerUri    = "tcp://138.229.113.84:1883";	
         String clientId     = "JavaSample_sender";									//测试中会看查这个是否会有影响字节数!!!!!!!!!!!!!!!!!!!
         
         String myuserName	= "IamPublisherOne";
@@ -86,7 +91,92 @@ public class TestMain_Auth_SubOffl_SubOnl {
 		int statusUpdateMaxTimes = 35;
 		int statusUpdate = 0;
 
+		
+		
+		
+		
+		//ref:https://github.com/eclipse/paho.mqtt.java/blob/6f35dcb785597a6fd49091efe2dba47513939420/org.eclipse.paho.mqttv5.client.test/src/test/java/org/eclipse/paho/mqttv5/common/packet/MqttConnectTest.java
+		// paho.mqtt.java/org.eclipse.paho.mqttv5.client.test/src/test/java/org/eclipse/paho/mqttv5/common/packet/MqttConnectTest.java 
+		//String clientId = "testClientId";
+		int mqttVersion = 5;
+		boolean cleanStart = true;
+		int keepAliveInterval = 60;
+		String userName = "username";
+		 byte[] password = "password".getBytes();
+		 String willPayload = "Will Message";
+		 String willDestination = "will/destination";
+		 int willQoS = 1;
+
+		 Long sessionExpiryInterval = 60L;
+		 Long maxPacketSize = 128000L;
+		 Integer topicAliasMax = 5;
+		 boolean requestResponseInfo = true;
+		 boolean requestPropblemInfo = true;
+		 String authMethod = "PASSWORD";
+		 byte[] authData = "secretPassword123".getBytes();
+		 String userKey1 = "userKey1";
+		 String userKey2 = "";
+		 String userKey3 = "userKey3";
+		 String userKey4 = "";
+		 String userValue1 = "userValue1";
+		 String userValue2 = "userValue2";
+		 String userValue3 = "";
+		 String userValue4 = "";
+
+		 Long willDelayInterval = 3L;			//关掉sender 等3秒后 broker 会把 will topic和它的data 发送给 订阅了   这个willtopic的subscrbier
+		 boolean willIsUTF8 = true;
+		 Long willPublicationExpiryInterval = 60L;
+		 String willResponseTopic = "replyTopic";
+		 byte[] willCorrelationData = "correlationData".getBytes();			//设置为"" 看字节数
+		 String willContentType = "sssssJSON";										//设置为"" 看字节数
+		
+		
+		
         try {
+    		MqttProperties properties = new MqttProperties();
+    		MqttProperties willProperties = new MqttProperties();
+    		MqttMessage willMessage = new MqttMessage(willPayload.getBytes());
+    		willMessage.setQos(willQoS);
+
+    		properties.setSessionExpiryInterval(sessionExpiryInterval);
+    		properties.setMaximumPacketSize(maxPacketSize);
+    		properties.setTopicAliasMaximum(topicAliasMax);
+    		properties.setRequestResponseInfo(requestResponseInfo);
+    		properties.setRequestProblemInfo(requestPropblemInfo);
+    		properties.setAuthenticationMethod(authMethod);
+    		properties.setAuthenticationData(authData);
+
+    		ArrayList<UserProperty> userDefinedProperties = new ArrayList<UserProperty>();
+    		userDefinedProperties.add(new UserProperty(userKey1, userValue1));
+    		userDefinedProperties.add(new UserProperty(userKey2, userValue2));
+    		userDefinedProperties.add(new UserProperty(userKey3, userValue3));
+    		userDefinedProperties.add(new UserProperty(userKey4, userValue4));
+    		properties.setUserProperties(userDefinedProperties);
+
+    		willProperties.setMessageExpiryInterval(willPublicationExpiryInterval);
+    		willProperties.setWillDelayInterval(willDelayInterval);
+    		willProperties.setPayloadFormat(willIsUTF8);
+    		willProperties.setContentType(willContentType);
+    		willProperties.setResponseTopic(willResponseTopic);
+    		willProperties.setCorrelationData(willCorrelationData);
+
+    		willProperties.setUserProperties(userDefinedProperties);
+
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
         	//MqttAsyncClient sampleClient = new MqttAsyncClient(brokerUri, clientId, new MqttDefaultFilePersistence());
         	MqttClient sampleClient = new MqttClient(brokerUri, clientId, new MemoryPersistence());
         	
@@ -108,9 +198,36 @@ public class TestMain_Auth_SubOffl_SubOnl {
             connOpts.setUserName(myuserName);
             connOpts.setPassword(mypwd.getBytes());
 
+            
+            
+            
+            
+            
+            
+            
+            /*
+            //用来看authdata 占多少字节  但是 好像暂时不能运行, 只是说可以用来看包大小
+            connOpts.setAuthMethod(authMethod);
+            connOpts.setAuthData(authData);
+            */
+            
+            
+            connOpts.setUserProperties(userDefinedProperties);
+            connOpts.setWillMessageProperties(willProperties);
+            connOpts.setWill(willResponseTopic, willMessage);
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
             // connect to broker
             sampleClient.connect(connOpts);											//如果是MqttClient 贼需要这个
-            //sampleClient.connect(connOpts, null, null).waitForCompletion(-1); 			//如果是MqttAsyncClient 贼需要这个
+            //sampleClient.connect(connOpts, null, null).waitForCompletion(-1); 	//如果是MqttAsyncClient 贼需要这个
             //
             MqttMessage message_tmp=null;
             StringBuffer str_content_tmp = new StringBuffer("");
@@ -127,7 +244,19 @@ public class TestMain_Auth_SubOffl_SubOnl {
        
             	System.out.println("published:"+str_content_tmp);
                 sampleClient.publish(topic, message_tmp);
-                //sampleClient.publish(topic+"2", message_tmp);
+                sampleClient.publish(topic+"2", message_tmp);
+                sampleClient.publish(topic+"3", message_tmp);
+                sampleClient.publish(topic+"4", message_tmp);
+                sampleClient.publish(topic+"5", message_tmp);
+                sampleClient.publish(topic+"6", message_tmp);
+                sampleClient.publish(topic+"7", message_tmp);
+                sampleClient.publish(topic+"8", message_tmp);
+                sampleClient.publish(topic+"9", message_tmp);
+                sampleClient.publish(topic+"10", message_tmp);
+                sampleClient.publish(topic+"11", message_tmp);
+                sampleClient.publish(topic+"12", message_tmp);
+                
+                
                 //
                 Thread.sleep(1000);
             }
